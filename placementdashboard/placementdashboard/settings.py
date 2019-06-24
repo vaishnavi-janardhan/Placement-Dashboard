b0f97c7a484815ec91518baaf6c37b4174ea4553
Vaishnavi Janardhan <vaish.janardhan@gmail.com>
2019-06-24 12:13:27 +0530

    Initial setup

--- placementdashboard/placementdashboard/settings.py
+++ placementdashboard/placementdashboard/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'placementdashboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
