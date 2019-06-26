a70002d4ce10cb942e0472c02efc2d0047e26323
Bhumika Saxena <bhumika056btcse17@igdtuw.ac.in>
2019-06-26 09:59:09 +0530

    Made changes

--- placementdashboard/dashboard/urls.py
+++ placementdashboard/dashboard/urls.py
@@ -6,6 +6,6 @@ app_name = 'Placement Dashboard'
 
 urlpatterns = [
     path('', views.user_login, name='user_login'),
-    # url(r'^user_login/$',views.user_login,name='user_login')
+    url(r'^welcome/$',views.index,name='index'),
     url('csv', views.getfile, name = 'download_csv')
 ]
